2a96e1c5cea945d314aba143665fcf6b5800101b
Jason Nam <neep305@gmail.com>
2019-02-20 20:43:06 +0900

    router 경로 수정

--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,13 @@ var router = express.Router();
 var db = require('../lib/sybase-conn');
 
 /* GET home page. */
-router.get('/', async (req, res, next) => {
+router.get('/result', async function(req, res, next) {
   try {
     console.log('req.params', req.params);
 
     const data = await db.getAllSrData(req.params);
     console.log('data : ', data);
-    
+
     var result = new Object();
     result.result = 'bad';
     res.status(200).json(JSON.stringify(result));  
