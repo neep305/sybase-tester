b8ead26583f7e59d241016e211aac3340d19fcf7
Jason Nam <neep305@gmail.com>
2019-02-20 10:49:07 +0900

    Sybase 설정 추가

---
+++ index.js
@@ -0,0 +1,14 @@
+var Sybase = require('sybase');
+var db = new Sybase('165.243.210.104',5638,'gshsdw','bs','bsuser');
+
+db.connect((err) => {
+    if (err) return console.log(err);
+
+    db.query('select MD_ID from lghs.D_GOODS', (err, data) => {
+        if (err) console.log(err);
+
+        console.log(data);
+
+        db.disconnect();
+    });
+});
